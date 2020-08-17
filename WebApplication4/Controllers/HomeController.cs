5539798bcd21056e7d00c636e4788f2a175e6863
ece bükcü <ecebukcu@hotmail.com>
2020-08-17 18:28:43 +0300

    eecee

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -27,7 +27,7 @@ namespace WebApplication4.Controllers
             ClaimsPrincipal cp = this.User;
             String cDisplayName = cp.FindFirst(ClaimTypes.Name).Value;
             ViewBag.userName = cDisplayName;
-            int a = 5;
+            int a = 55;
             return View();
         }
 
